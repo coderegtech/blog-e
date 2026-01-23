1674e7cee0541fab894824927ba3c154afb8a601
coderegtech <reygundanag55@gmail.com>
2026-01-23 09:14:11 +0800

    feat: added try catch block

--- features/blog/blogThunks.ts
+++ features/blog/blogThunks.ts
@@ -4,71 +4,91 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const createBlogAsync = createAsyncThunk(
   "blog/createBlogAsync",
   async (blog: Blog) => {
-    const response = await fetch(`/api/blog`, {
-      method: "POST",
-      body: JSON.stringify(blog),
-    });
+    try {
+      const response = await fetch(`/api/blog`, {
+        method: "POST",
+        body: JSON.stringify(blog),
+      });
 
-    if (!response.ok) {
-      return [];
+      if (!response.ok) {
+        return [];
+      }
+      const responseData = await response.json();
+      return responseData?.data;
+    } catch (error) {
+      throw error;
     }
-    const responseData = await response.json();
-    return responseData?.data;
   },
 );
 
 export const getBlogsAsync = createAsyncThunk(
   "blog/getBlogsAsync",
   async () => {
-    const response = await fetch("/api/blog");
-    if (!response.ok) {
-      return [];
+    try {
+      const response = await fetch("/api/blog");
+      if (!response.ok) {
+        return [];
+      }
+      console.log("from thunks: ", response);
+      const responseData = await response.json();
+      return responseData?.data;
+    } catch (error) {
+      throw error;
     }
-    console.log("from thunks: ", response);
-    const responseData = await response.json();
-    return responseData?.data;
   },
 );
 
 export const getUserBlogsAsync = createAsyncThunk(
   "blog/getUserBlogsAsync",
   async (uid: string) => {
-    const response = await fetch(`/api/blog/user/${uid}`);
-    if (!response.ok) {
-      return [];
+    try {
+      const response = await fetch(`/api/blog/user/${uid}`);
+      if (!response.ok) {
+        return [];
+      }
+      const responseData = await response.json();
+      return responseData?.data;
+    } catch (error) {
+      throw error;
     }
-    const responseData = await response.json();
-    return responseData?.data;
   },
 );
 
 export const editBlogAsync = createAsyncThunk(
   "blog/editBlogAsync",
   async (blog: Blog) => {
-    const response = await fetch(`/api/blog/${blog.id}`, {
-      method: "PATCH",
-      body: JSON.stringify(blog),
-    });
+    try {
+      const response = await fetch(`/api/blog/${blog.id}`, {
+        method: "PATCH",
+        body: JSON.stringify(blog),
+      });
 
-    if (!response.ok) {
-      return [];
+      if (!response.ok) {
+        return [];
+      }
+      const responseData = await response.json();
+      return responseData?.data;
+    } catch (error) {
+      throw error;
     }
-    const responseData = await response.json();
-    return responseData?.data;
   },
 );
 
 export const deleteBlogAsync = createAsyncThunk(
   "blog/deleteBlogAsync",
   async (id: string) => {
-    const response = await fetch(`/api/blog/${id}`, {
-      method: "DELETE",
-    });
+    try {
+      const response = await fetch(`/api/blog/${id}`, {
+        method: "DELETE",
+      });
 
-    if (!response.ok) {
-      return [];
+      if (!response.ok) {
+        return [];
+      }
+      const responseData = await response.json();
+      return responseData?.data;
+    } catch (error) {
+      throw error;
     }
-    const responseData = await response.json();
-    return responseData?.data;
   },
 );
